5c6d485f28564f08dd17ee1821401af0a9c73b7c
一宁 <596469642@qq.com>
2019-08-18 15:07:52 +0800

    第三版个性网头像

--- paaaaaaaa.py
+++ paaaaaaaa.py
@@ -1,4 +1,4 @@
-import requests, hashlib, os,logging
+import requests, hashlib, os, logging
 from lxml import etree
 from fake_useragent import UserAgent
 
@@ -13,23 +13,30 @@ for l in range(400):
     for i in range(1, 3):
         for k in range(1, 10):
             u_a = UserAgent().random
+
+
             def headers():
                 # 配置请求头
                 header = {
                     "User-Agent": "{}".format(u_a),
                 }
                 return header
-            photo_list = requests.get("https://www.gexing.com/qqtouxiang/new/{}".format(l),headers=headers())
-            response = photo_list.content.decode()
-            html = etree.HTML(response)
-            big_photo = html.xpath('//*[@id="tx_post_list"]/ul[{}]/li[{}]/div[1]/a/@href'.format(i, k))
-            num = str(big_photo[0])
-            number = num.strip("https://www.gexing.com/qqtouxiang/")
-            number = number.strip(".html")
-            print(number)
-            name = requests.get(big_photo[0],headers=headers())
-            p = name.content.decode()
-            md5 = hashlib.md5()
+
+
+            try:
+                photo_list = requests.get("https://www.gexing.com/qqtouxiang/new/{}".format(l), headers=headers())
+                response = photo_list.content.decode()
+                html = etree.HTML(response)
+                big_photo = html.xpath('//*[@id="tx_post_list"]/ul[{}]/li[{}]/div[1]/a/@href'.format(i, k))
+                num = str(big_photo[0])
+                number = num.strip("https://www.gexing.com/qqtouxiang/")
+                number = number.strip(".html")
+                print(number)
+                name = requests.get(big_photo[0], headers=headers())
+                p = name.content.decode()
+                md5 = hashlib.md5()
+            except Exception as e:
+                logging.debug("这是第一条报错信息::::::{}".format(e))
 
             try:
                 for j in range(1, 31):
@@ -37,7 +44,7 @@ for l in range(400):
                         html = etree.HTML(p)
                         photo = html.xpath('//*[@id="119087{}{}"]/img/@src'.format(number, j))
                         if photo:
-                            da = requests.get(photo[0],headers=headers())
+                            da = requests.get(photo[0], headers=headers())
                             da = da.content
                             md5.update(da)
                             name1 = md5.hexdigest()
@@ -50,12 +57,13 @@ for l in range(400):
                         else:
                             break
                     if 9 < j < 22:
-                        photo_2 = requests.get('https://www.gexing.com/qqtouxiang/{}_2.html'.format(number),headers=headers())
+                        photo_2 = requests.get('https://www.gexing.com/qqtouxiang/{}_2.html'.format(number),
+                                               headers=headers())
                         photo_2 = photo_2.content.decode()
                         html = etree.HTML(photo_2)
                         photo_2 = html.xpath('//*[@id="119087{}{}"]/img/@src'.format(number, j))
                         if photo_2:
-                            da = requests.get(photo_2[0],headers=headers())
+                            da = requests.get(photo_2[0], headers=headers())
                             da = da.content
                             md5.update(da)
                             name1 = md5.hexdigest()
@@ -68,12 +76,13 @@ for l in range(400):
                         else:
                             break
                     elif j > 21:
-                        photo_3 = requests.get('https://www.gexing.com/qqtouxiang/{}_3.html'.format(number),headers=headers())
+                        photo_3 = requests.get('https://www.gexing.com/qqtouxiang/{}_3.html'.format(number),
+                                               headers=headers())
                         photo_3 = photo_3.content.decode()
                         html = etree.HTML(photo_3)
                         photo_3 = html.xpath('//*[@id="119087{}{}"]/img/@src'.format(number, j))
                         if photo_3:
-                            da = requests.get(photo_3[0],headers=headers())
+                            da = requests.get(photo_3[0], headers=headers())
                             da = da.content
                             md5.update(da)
                             name1 = md5.hexdigest()
@@ -86,5 +95,4 @@ for l in range(400):
                         else:
                             continue
             except Exception as e:
-                logging.debug("这是一条报错信息:::{}".format(e))
-
+                logging.debug("这是第二条报错信息:::{}".format(e))
